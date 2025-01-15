df4f734db263a5ab77a80e20f536d705adb1fc59
Joseph Heinz <josephheinz28@gmail.com>
2025-01-15 15:35:06 +0000

    added students out to home screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
   import Container from "../Container.svelte";
   import Navbar from "../Navbar.svelte";
   import { navLinks, students, Student, getAllStudents } from "../store";
+  import StudentContainer from "../StudentContainer.svelte";
+  import StudentsContainer from "../StudentsContainer.svelte";
 
   getAllStudents();
 </script>
@@ -11,4 +13,14 @@
 
 <Window title={"Student Tracker Dashboard"} id={"tracker-main"}>
   <Navbar {navLinks} />
+  <h1 class="
+    sm:font-bold sm:text-xl
+  ">Students Currently Out:</h1>
+  <StudentsContainer>
+    {#each $students as student}
+      {#if student?.currentPunchout !== null}
+        <StudentContainer {student}/>
+      {/if}
+    {/each}
+  </StudentsContainer>
 </Window>
